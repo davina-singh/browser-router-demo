8b826a9c8b3491ba9d5c30d49218ae8c1fa7f71e
DAVINA SINGH <davina_singh@outlook.com>
2023-10-27 11:35:01 +0100

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+export default function Home() {
+    return (
+    <main>
+        <h2>Home Page</h2>
+        <p>
+            React Router sllows us to have multiple pages in our SPA (Single Page Application)
+        </p>
+     </main>   
+    );
+}
